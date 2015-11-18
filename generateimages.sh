176525bb5e1e192e2fb5d42f28e938cbaa115c5d
Jonne Airaksinen <jonne.airaksinen@solinor.com>
2015-11-18 09:13:57 +0100

    Fixed json generator.

--- generateimages.sh
+++ generateimages.sh
@@ -2,7 +2,7 @@ mkdir -p thumbs
 echo "\"images\":[" >> images.json
 for f in img/*.jpg
 do
-	echo "\"${f##*/}',\" >> images.json
+	echo "\"${f##*/}\"," >> images.json
   convert -thumbnail 200 $f thumbs/thumb-${f##*/}
 done
 
