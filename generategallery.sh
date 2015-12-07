63a5de45f7eaa70bc1e4cf50141592972cb81a63
Jonne Airaksinen <jonne.airaksinen@solinor.com>
2015-12-07 16:54:55 +0100

    Added spinner to image while loading.

--- generategallery.sh
+++ generategallery.sh
@@ -5,6 +5,7 @@ repodir=`dirname $0`
 ln -sf $repodir/html/index.html index.html
 ln -sf $repodir/html/reset.css reset.css
 ln -sf $repodir/html/script.js script.js
+ln -sf $repodir/html/spin.min.js spin.min.js
 ln -sf $repodir/html/style.css style.css
 
 mkdir -p thumbs
--- html/script.js
+++ html/script.js
@@ -5,6 +5,7 @@ var thumbnailContainerWidth = 0;
 var thumbnailContainerWidthSet = false;
 var images;
 var queue = [];
+var spinner;
 
 $(document).ready(function() {
 
@@ -167,4 +168,40 @@ function addToQueue(image) {
     }
   }
   queue.push(image);
+}
+
+function createSpinner(element) {
+    var opts = {
+        lines: 13, // The number of lines to draw
+        length: 7, // The length of each line
+        width: 3, // The line thickness
+        radius: 8, // The radius of the inner circle
+        corners: 0.9, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1.4, // Rounds per second
+        trail: 86, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '50%', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+
+    spinnerElement = $(element);
+
+    if (spinner) {
+        spinner.stop();
+    }
+    spinnerElement.addClass('spinner-active');
+    spinnerElement.prop('disabled', true);
+    spinner = new Spinner(opts).spin(element);
+}
+
+function stopSpinner() {
+    spinnerElement.removeClass('spinner-active');
+    spinnerElement.prop('disabled', false);
+    spinner.stop();
 }
