c5cebc906f0933b4d714307cb4d16b02b9d98dba
Jonne Airaksinen <jonne.airaksinen@solinor.com>
2015-12-07 17:13:14 +0100

    Fixing spinner script.

--- html/script.js
+++ html/script.js
@@ -175,14 +175,14 @@ function addToQueue(image) {
 function createSpinner(element) {
     var opts = {
         lines: 13, // The number of lines to draw
-        length: 7, // The length of each line
-        width: 3, // The line thickness
-        radius: 8, // The radius of the inner circle
+        length: 18, // The length of each line
+        width: 7, // The line thickness
+        radius: 15, // The radius of the inner circle
         corners: 0.9, // Corner roundness (0..1)
         rotate: 0, // The rotation offset
         direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#000', // #rgb or #rrggbb or array of colors
-        speed: 1.4, // Rounds per second
+        color: '#fafafa', // #rgb or #rrggbb or array of colors
+        speed: 1.2, // Rounds per second
         trail: 86, // Afterglow percentage
         shadow: false, // Whether to render a shadow
         hwaccel: false, // Whether to use hardware acceleration
@@ -192,14 +192,12 @@ function createSpinner(element) {
         left: '50%' // Left position relative to parent
     };
 
-    spinnerElement = $('.image-container');
-
     if (spinner) {
         spinner.stop();
     }
-    spinnerElement.addClass('spinner-active');
-    spinnerElement.prop('disabled', true);
-    spinner = new Spinner(opts).spin(element);
+
+    var target = document.getElementsByClassName('image-container')[0];
+    spinner = new Spinner(opts).spin(target);
 }
 
 function stopSpinner() {
